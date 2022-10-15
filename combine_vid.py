5dfa16b85e00fd303c629be6a3473dd81b9b617c
SakkyOP <saksham298@gmail.com>
2022-10-15 20:08:14 +0530

    fragmented frames into batches before compiling

--- combine_vid.py
+++ combine_vid.py
@@ -1,5 +1,5 @@
-
 import os
+import shutil
 from moviepy.editor import *
 
 """
@@ -16,15 +16,32 @@ out_video.write_videofile("video.avi", fps=23.5, codec= 'rawvideo')
 """
 def clear_dir(inPath):
     for file in os.listdir(inPath):
-        os.unlink(os.path.join(inPath, file))
+        shutil.rmtree(os.path.join(inPath,file))
 
 def save_video(inPath, outPath, originalVideoPath, fps):
-    images = []
+    temp_vid_dir = ".\\data\\temp_vids"
+    os.makedirs(temp_vid_dir, exist_ok= True)
     
-    for i in range(len(os.listdir(inPath))):
-        images.append(ImageClip(os.path.join(inPath, os.listdir(inPath)[i])).set_duration(1/fps))
+    collections = [ os.path.join(inPath, collection) for collection in os.listdir(inPath) ]
+    
+    for collection in collections:
+        images = [ os.path.join(collection, image) for image in os.listdir(collection) ]
+        images_to_video(images, fps, temp_vid_dir+"\\temp_vid_"+str(collections.index(collection))+".avi")
 
-    out_video = concatenate_videoclips(images, method= 'compose')
+    clips = [ VideoFileClip(os.path.join(temp_vid_dir, clip)) for clip in os.listdir(temp_vid_dir) ]
+
+    out_video = concatenate_videoclips([*clips])
     out_video.write_videofile(outPath, fps=fps, codec= 'rawvideo')
     
     VideoFileClip(outPath).set_audio(VideoFileClip(originalVideoPath).audio)
+
+def images_to_video(frames, fps, outPath):
+    
+    vid_frames = []
+    
+    for frame in frames:
+        vid_frames.append(ImageClip(frame).set_duration(1/fps))
+        
+    out_video = concatenate_videoclips(vid_frames, method = "compose")
+    out_video.write_videofile(outPath, fps= fps, codec = "rawvideo")
+    
--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import os
 import cv2
+import traceback # for error handling
 from moviepy.editor import VideoFileClip
 from upscaler import upscale_image
 from combine_vid import save_video, clear_dir
 
 CURRENT_DIRECTORY = os.getcwd()
 
-IN_VIDEO = ".\\data\\in_video\\video_3.mp4"
-OUT_VIDEO = ".\\data\\out_video\\upscale_video_1.avi"
+IN_VIDEO = ".\\data\\in_video"
+OUT_VIDEO = ".\\data\\out_video"
 OUT_FRAMES = ".\\data\\out_frames"
+[os.makedirs(dir, exist_ok=True) for dir in [IN_VIDEO, OUT_VIDEO, OUT_FRAMES]]
+
+IN_VIDEO = IN_VIDEO + "\\video_3.mp4"
+OUT_VIDEO = OUT_VIDEO + "\\upscale_video_1.avi"
+
 MODEL_PATH = os.path.join(CURRENT_DIRECTORY,"models","FSRCNN-small_x4.pb")
 MODEL_NAME = "fsrcnn"
 SCALE = 4
@@ -29,7 +35,10 @@ try:
         
         up_frame = upscale_image(frame, MODEL_PATH, MODEL_NAME, SCALE)
 
-        cv2.imwrite(OUT_FRAMES+"\\frame_no_{0}.jpg".format(frame_count), up_frame)
+        collectiondir = OUT_FRAMES+ f"\\collection{frame_count // 100}"
+        os.makedirs(collectiondir, exist_ok= True)
+        
+        cv2.imwrite(collectiondir+"\\frame_no_{0}.jpg".format(frame_count, frame_count % 100), up_frame)
         
         print(f"No. of Frames processed {frame_count}")
 
@@ -38,8 +47,14 @@ except KeyboardInterrupt:
     
 except Exception as e:
     print("An error occurred: {0}".format(e))
+    print(traceback.format_exc())
 
-save_video(OUT_FRAMES, OUT_VIDEO, IN_VIDEO, FPS)
+try:
+    save_video(OUT_FRAMES, OUT_VIDEO, IN_VIDEO, FPS)
+except Exception as e:
+    print("An error occurred: {0}".format(e))
+    print(traceback.format_exc())
+
 video.release()
 
 clear_dir(OUT_FRAMES)
